7baa6f8ff132dea91edc95ad7f23e7d9d9f8bcec
Muamer Aličković <75814679+mr-am2k@users.noreply.github.com>
2022-07-08 18:26:59 +0200

    Features section completed

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import './possiblity.css'
+import './possibility.css'
+import possibilityImage from '../../assets/possibilityImage.png'
 
 const Possibility = () => {
+  const titleContent = 'The possibilities are beyond your imagination'
+  const paragraphContent = 'Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.'
+
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img  src={possibilityImage} alt='Possibility' />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>{titleContent}</h1>
+        <p>{paragraphContent}</p>
+        <h4>Request Early Access to get Started</h4>
+      </div>
+    </div>
   )
 }
 
